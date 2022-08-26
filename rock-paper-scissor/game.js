813a08bb6ce9b4a202014fb9ec5d0d31be62c52e
Jordan Luse <j.luse7@gmail.com>
2022-08-25 19:43:41 -0700

    Need to account for best out of 3 game

--- rock-paper-scissor/game.js
+++ rock-paper-scissor/game.js
@@ -1,4 +1,9 @@
 let oneRound = document.querySelector('.btn');
+let multiRound = document.querySelector('.btn-1');
+
+let userScore = 0;
+let botScore = 0;
+
 const SELECTIONS = [
   {
     name: 'rock',
@@ -14,36 +19,65 @@ const SELECTIONS = [
   }
 ]
 
-
-
-oneRound.addEventListener("click", e => {
-  let input = prompt('rock, paper, scissor?');
-  let valid = false;
-
-  if (input === 'rock' || input === 'paper' || input === 'scissor') {
-    valid = true;
+let validatePicks = function (userInput, validation) {
+  if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissor') {
+    validation = true;
   } else {
     alert('Type in rock, paper or scissor')
   }
+  return validation;
+}
 
+oneRound.addEventListener("click", e => {
+  let input = prompt('rock, paper, scissor?').toLowerCase();
+  let valid = false;
   let userSelection = SELECTIONS.find(selection => selection.name === input)
 
-  if (valid) {
+  if (validatePicks(input, valid)) {
     runGame(userSelection)
   }
 });
 
-function runGame(selection) {
+multiRound.addEventListener("click", e => {
+  let rounds = 0;
+  let valid = false;
+
+  do {
+    let input = prompt('rock, paper, scissor?').toLowerCase();
+    let userSelection = SELECTIONS.find(selection => selection.name === input)
+
+    if (validatePicks(input, valid)) {
+      runGame(userSelection)
+    }
+
+    // The best out of three mode should only stop when either user or the bot player has won at least two rounds.
+    // Maybe add if condtion to account for this, only progress round unless someone has score >= 2
+    //
+    rounds += 1
+
+  } while (rounds < 4);
+
+  if (userScore > botScore) {
+    alert('YOU WIN THE GAME! Your Score: ' + userScore + ' BOT score: ' + botScore);
+  } else if (userScore < botScore) {
+    alert('LOST THE GAME TO DA BOT! Your Score:' + userScore + ' BOT score: ' + botScore);
+  } else if (userScore === botScore) {
+    alert('THE GAME WAS A TIE. Your Score:' + userScore + ' BOT score: ' + botScore);
+  }
+});
+
+let runGame = function (selection) {
   // console.log(selection);
+
   let botChoice = SELECTIONS[Math.floor(Math.random() * SELECTIONS.length)];
+
   if (selection.name === botChoice.name) {
-    // console.log('TIE, Bot: ' + botChoice.name + " USER: " + selection.name);
     alert('TIE! Bot: ' + botChoice.name + " USER: " + selection.name)
   } else if (selection.name === botChoice.defeats) {
-    // console.log('LOST, Bot: ' + botChoice.name + " USER: " + selection.name);
+    botScore++;
     alert('YOU LOST! Bot: ' + botChoice.name + " USER: " + selection.name)
   } else if (selection.defeats === botChoice.name){
-    // console.log('YOU WON! Bot: ' + botChoice.name + " USER: " + selection.name);
+    userScore++;
     alert('YOU WON! Bot: ' + botChoice.name + " USER: " + selection.name)
   }
 }
