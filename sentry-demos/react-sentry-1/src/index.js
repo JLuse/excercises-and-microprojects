62c7910b360348bf7891756d91281300954d85f5
Jordan <j.luse7@gmail.com>
2023-01-18 21:23:18 -0800

    Testing Sentry releases

--- sentry-demos/react-sentry-1/src/index.js
+++ sentry-demos/react-sentry-1/src/index.js
@@ -8,11 +8,9 @@ Sentry.init({
   dsn: "https://3aa7d589de824a9d9a867d37aff73a87@o565143.ingest.sentry.io/4504523602722816",
   integrations: [new BrowserTracing()],
   enviroment: 'dev',
-  release: 'react-sentry-@0.0.2',
+  // release: 'react-sentry@0.0.5',
   tracesSampleRate: 1.0,
 });
-
-console.log(process.env.npm_package_version)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
