6a0d25d7a8d5054fffda24453829f04eadcd80fe
Jordan Luse <j.luse7@gmail.com>
2023-02-23 18:04:15 -0800

    Pushing changes to use laptop

--- sentry-demos/tracing-backend-node/server.js
+++ sentry-demos/tracing-backend-node/server.js
@@ -11,7 +11,7 @@ Sentry.init({
     new Sentry.Integrations.Http({ tracing: true }),
     // enable Express.js middleware tracing
     new Tracing.Integrations.Express({
-      // to trace all requests to the default router
+      // to trac requests to the default router
       app,
       // alternatively, you can specify the routes you want to trace:
       // router: someRouter,
