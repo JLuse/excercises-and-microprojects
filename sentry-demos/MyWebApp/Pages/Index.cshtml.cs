7646396364c86c288e3980cbf4c0e5fe31ac920b
Jordan <j.luse7@gmail.com>
2023-03-15 20:50:43 -0700

    Added .net project

---
+++ sentry-demos/MyWebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Sentry.AspNetCore;
+using Sentry;
+
+namespace MyWebApp.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+        SentrySdk.CaptureMessage("Hello Sentry");
+    }
+}
---
+++ sentry-demos/MyWebApp/Program.cs
@@ -0,0 +1,25 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.UseSentry();
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.UseSentryTracing();
+
+app.Run();
