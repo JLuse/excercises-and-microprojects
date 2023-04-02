515e93cd28081862927ab727d4d4c5fff2d6cebc
Jordan Luse <j.luse7@gmail.com>
2023-04-02 13:52:33 -0700

    Added full stack part1 progress

--- fullstack-open/part1/src/App.js
+++ fullstack-open/part1/src/App.js
@@ -2,50 +2,54 @@ import Header from "./Header"
 import Content from "./Content"
 import Total from "./Total"
 
+// part 1b
+// const App = () => {
+//   const course = 'Half Stack application development'
+//   const parts = [
+//     {
+//       name: 'Fundamentals of React',
+//       exercises: 10
+//     },
+//     {
+//       name: 'Using props to pass data',
+//       exercises: 7
+//     },
+//     {
+//       name: 'State of a component',
+//       exercises: 14
+//     }
+//   ]
 
-const App = () => {
-  // const course = 'Half Stack application development'
-  // const part1 = 'Fundamentals of React'
-  // const exercises1 = 10
-  // const part2 = 'Using props to pass data'
-  // const exercises2 = 7
-  // const part3 = 'State of a component'
-  // const exercises3 = 14
-  const course = 'Half Stack application development'
-  // const part1 = {
-  //   name: 'Fundamentals of React',
-  //   exercises: 10
-  // }
-  // const part2 = {
-  //   name: 'Using props to pass data',
-  //   exercises: 7
-  // }
-  // const part3 = {
-  //   name: 'State of a component',
-  //   exercises: 14
-  // }
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+//   return (
+//   <div>
+//     <Header course={course} />
+//     <Content parts={parts} />
+//     <Total parts={parts} />
+//   </div>
+//   )
+// }
 
+// Part 1c
+// const Hello = ({name, age}) => {
+//   const bornYear = () => new Date().getFullYear() - age
+
+//   return (
+//     <div>
+//       <p>
+//         Hello {name}, you are {age} years old
+//       </p>
+//       <p>So you were probably born in {bornYear()}</p>
+//     </div>
+//   )
+// }
+const App = (props) => {
+  const {counter} = props
   return (
-  <div>
-    <Header course={course} />
-    <Content parts={parts} />
-    <Total parts={parts} />
-  </div>
+    <div>
+      {counter}
+    </div>
   )
 }
+
 
 export default App
