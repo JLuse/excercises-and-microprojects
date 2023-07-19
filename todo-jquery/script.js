a72c93e34bada694f5d0f4be1bc5dc7a36c6bb6e
Jordan Luse <j.luse7@gmail.com>
2023-07-18 20:34:05 -0700

    Pushing to switch over to laptop

--- todo-jquery/script.js
+++ todo-jquery/script.js
@@ -1,3 +1,10 @@
 // GET Your todo list by default when the DOM is ready, no need for inout for your 
 // from there all methods and actions
 // create JS functions for each method/task (GET, POST, UPDATE, DELETE)
+let getGoals = function() {
+  console.log('GET GOALS, jquery')
+}
+
+$(document).ready(function () {
+
+});
