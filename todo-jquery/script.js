7a03b455a03e52d3f52544c023f521b15af4d2bf
Jordan Luse <j.luse7@gmail.com>
2023-07-18 21:22:00 -0700

    Re arrange content to microproject repo

--- todo-jquery/script.js
+++ todo-jquery/script.js
@@ -1,10 +1,43 @@
 // GET Your todo list by default when the DOM is ready, no need for inout for your 
 // from there all methods and actions
 // create JS functions for each method/task (GET, POST, UPDATE, DELETE)
+// my user id - https://fewd-todolist-api.onrender.com/tasks?api_key=233
+
 let getGoals = function() {
-  console.log('GET GOALS, jquery')
+  var $goalList = $('#goalList')
+  // console.log($goalList)
+
+  $.ajax({
+    type: 'GET',
+    url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=233',
+    dataType: 'json',
+    success: function (response, textStatus) {
+      var goals = response.tasks
+      console.log(goals);
+      if (goals.length > 0) {
+        console.log('true')
+        goals.forEach(function(goal) {
+          var goalItem = document.createElement('li')
+          goal.className = 'row goal-item'
+
+        })
+      } else {
+        console.log('false')
+      }
+      // response is a parsed JavaScript object instead of raw JSON
+
+    },
+    error: function (request, textStatus, errorMessage) {
+      console.log(errorMessage);
+    }
+  })
 }
+// Add goal
+$('#form').submit(function(event){
+  event.preventDefault();
+  console.log('ADD')
+});
 
 $(document).ready(function () {
-
-});
+  getGoals()
+})
