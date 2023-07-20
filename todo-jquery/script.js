e23bde0ada7591881bc9f4539f711358d1b2bbe1
Jordan <j.luse7@gmail.com>
2023-07-19 22:05:18 -0700

    Get all goals and add goal functionaility added

--- todo-jquery/script.js
+++ todo-jquery/script.js
@@ -2,20 +2,21 @@
 // from there all methods and actions
 // create JS functions for each method/task (GET, POST, UPDATE, DELETE)
 // my user id - https://fewd-todolist-api.onrender.com/tasks?api_key=233
+var goalList = document.getElementById('goalList')
 
 let getGoals = function() {
-  var goalList = document.getElementById('goalList')
-
+  goalList.innerHTML = ''
   $.ajax({
     type: 'GET',
     url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=233',
     dataType: 'json',
     success: function (response, textStatus) {
       var goals = response.tasks
-      console.log(goals);
+      // console.log(goals);
       if (goals.length > 0) {
         console.log('true')
         goals.forEach(function(goal) {
+          // console.log(goal.id)
           var goalItem = document.createElement('li')
           var completedContainer = document.createElement('div')
           var completedCheckBox = document.createElement('input')
@@ -29,32 +30,77 @@ let getGoals = function() {
           completedCheckBox.id = 'completed'
           completedCheckBox.type = 'checkbox'
           completedCheckBox.name = 'completed'
-          completedCheckBox.checked = false
           goalDescriptionContainer.className = 'col-xs-6'
           goalDescription.id = 'goalDescription'
           removeGoalContainer.className = 'col-xs-5'
           removeGoalButton.id = 'removeGoal'
+          removeGoalButton.setAttribute("data-id", goal.id);
+
+          completedCheckBox.checked = goal.completed
+          goalDescription.textContent = goal.content
+          removeGoalButton.textContent = 'Remove'
           
-          
+          goalList.appendChild(goalItem)
+          goalItem.appendChild(completedContainer)
+          goalItem.appendChild(goalDescriptionContainer)
+          goalItem.appendChild(removeGoalContainer)
 
-
-
+          completedContainer.appendChild(completedCheckBox)
+          goalDescriptionContainer.appendChild(goalDescription)
+          removeGoalContainer.appendChild(removeGoalButton)
         })
       } else {
         console.log('false')
       }
       // response is a parsed JavaScript object instead of raw JSON
-
     },
     error: function (request, textStatus, errorMessage) {
       console.log(errorMessage);
     }
   })
 }
+
 // Add goal
+let addGoal = function() {
+  $.ajax({
+    type: 'POST',
+    url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=233',
+    contentType: 'application/json',
+    dataType: 'json',
+    data: JSON.stringify({
+      task: {
+        content: document.getElementById('addGoal').value
+      }
+    }),
+    success: function (response, textStatus) {
+      console.log(response);
+      getGoals()
+
+    },
+    error: function (request, textStatus, errorMessage) {
+      console.log(errorMessage);
+    }
+  });
+}
+
+// remove goal
+let removeGoal = function() {
+  $.ajax({
+    type: 'DELETE',
+     url: 'https://fewd-todolist-api.onrender.com/tasks/5?api_key=233',
+     success: function (response, textStatus) {
+       console.log(response);
+     },
+     error: function (request, textStatus, errorMessage) {
+       console.log(errorMessage);
+     }
+   });
+}
+
+
 $('#form').submit(function(event){
   event.preventDefault();
-  console.log('ADD')
+  addGoal()
 });
 
 $(document).ready(function () {
