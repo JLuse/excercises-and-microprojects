6dfce9f5c41bc7cce6d2c5d41c7dc8e75afcfdd2
Jordan <j.luse7@gmail.com>
2023-08-22 20:30:16 -0700

    Radio buttons in place, but need to make fucntionalty change on each button pressed

--- maths-countdown/script.js
+++ maths-countdown/script.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
+  var addButton = document.getElementById('addition')
+  var subButton = document.getElementById('subtraction')
+  var multiplyButton = document.getElementById('multiplication')
+  var divisionButton = document.getElementById('division')
   var currentQuestion;
   var timeLeft = 10;
   var score = 0;
@@ -7,17 +11,46 @@ $(document).ready(function(){
   var randomNumberGenerator = function (size) {
     return Math.ceil(Math.random() * size);
   }
+
+  
   
   var questionGenerator = function () {
     var question = {};
     var num1 = randomNumberGenerator(10);
     var num2 = randomNumberGenerator(10);
     
-    question.answer = num1 + num2;
-    question.equation = String(num1) + " + " + String(num2);
+    if (addButton.checked) {
+      question.answer = num1 + num2;
+      question.equation = String(num1) + " + " + String(num2);
+      // return question;
+      console.log(question)
+    } else if (subButton.checked) {
+      question.answer = num1 - num2;
+      question.equation = String(num1) + " - " + String(num2);
+      // return question;
+      console.log(question)
+    } else if (multiplyButton.checked) {
+      question.answer = num1 * num2;
+      question.equation = String(num1) + " * " + String(num2);
+      // return question;
+      console.log(question)
+    } else if (divisionButton.checked) {
+      question.answer = num1 / num2;
+      question.equation = String(num1) + " / " + String(num2);
+      // return question;
+      console.log(question)
+    }
+
+    // question.answer = num1 + num2;
+    // question.equation = String(num1) + " + " + String(num2);
     
     return question;
   }
+
+  var subtract = function() {
+    questionGenerator()
+  }
+  subButton.addEventListener('click', subtract)
   
   currentQuestion = questionGenerator();
   $('#equation').text(currentQuestion.equation);
@@ -35,10 +68,6 @@ $(document).ready(function(){
       updateScore(+1);
     }
   };
-  
-  // $('#user-input').on('keyup', function () {
-  //   checkAnswer(Number($(this).val()), currentQuestion.answer);
-  // });
 
   var updateTimeLeft = function (amount) {
     timeLeft += amount;
@@ -50,16 +79,11 @@ $(document).ready(function(){
     $('#score').text(score);
   };
   
-  // var interval = setInterval(function () {
-  //   updateTimeLeft(-1);
-  //   if (timeLeft === 0) {
-  //     clearInterval(interval);
-  //   }
-  // }, 1000);
   var startGame = function () {
     if (!interval) {
       if (timeLeft === 0) {
         updateTimeLeft(10);
+        updateScore(-score);
       }
       interval = setInterval(function () {
         updateTimeLeft(-1);
@@ -76,7 +100,5 @@ $(document).ready(function(){
     checkAnswer(Number($(this).val()), currentQuestion.answer);
   });
 
-
   renderNewQuestion();
 }); // end dom ready
-
