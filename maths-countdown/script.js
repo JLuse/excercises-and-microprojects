5948d36dd31f4559c266b5cbd6aa52811d7c1eb8
Jordan <j.luse7@gmail.com>
2023-09-04 16:31:04 -0700

    Math game MVP

--- maths-countdown/script.js
+++ maths-countdown/script.js
@@ -7,6 +7,8 @@ $(document).ready(function(){
   var timeLeft = 10;
   var score = 0;
   var interval;
+  var giphy = document.getElementById('giphy')
+  var imgTag = document.createElement('img')
 
   var questionGenerator = function () {
     var question = {};
@@ -17,15 +19,30 @@ $(document).ready(function(){
     if (addButton.checked) {
       question.answer = num1 + num2;
       question.equation = String(num1) + " + " + String(num2);
+      imgTag.src = 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExd3lnZjluOGdldnh1NG1yY2hmaG0yN3NjejRyYnE1NnJweHplMmM1NyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3o6MbmXIt1qUsNzfu8/giphy.gif'
+      imgTag.alt = 'Math Giphy'
+      giphy.appendChild(imgTag)
+
     } else if (subButton.checked) {
       question.answer = num1 - num2;
       question.equation = String(num1) + " - " + String(num2);
+      imgTag.src = 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExMmF0aHRjdDI5eGJobWRpYWlkeHZxajV4Y3lzYjNvbGYyYXcxbDc2ZiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/gEvab1ilmJjA82FaSV/giphy.gif'
+      imgTag.alt = 'Math Giphy'
+      giphy.appendChild(imgTag)
+
     } else if (multiplyButton.checked) {
       question.answer = num1 * num2;
       question.equation = String(num1) + " * " + String(num2);
+      imgTag.src = 'https://media.giphy.com/media/FuSJ5C7SSHlZCxjC6q/giphy-downsized-large.gif'
+      imgTag.alt = 'Math Giphy'
+      giphy.appendChild(imgTag)
+
     } else if (divisionButton.checked) {
       question.answer = Math.floor(num1 / num2);
       question.equation = String(num1) + " / " + String(num2);
+      imgTag.src = 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNGw2c3JoNzAzejl2ZDdkMHVuc3B3YWtteG92NHNnMng3d3oxZHliYyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/ne3xrYlWtQFtC/giphy.gif'
+      imgTag.alt = 'Math Giphy'
+      giphy.appendChild(imgTag)
     }
     
     return question;
@@ -76,10 +93,17 @@ $(document).ready(function(){
     }
   }
 
+
   $('#user-input').on('keyup', function () {
     startGame();
     checkAnswer(Number($(this).val()), currentQuestion.answer);
   });
-
   renderNewQuestion();
 });
+
+// only allow numbers to be typed in
+var validateNumericInput = function (inputElement) {
+  var inputValue = inputElement.value;
+  inputValue = inputValue.replace(/[^0-9]/g, '');
+  inputElement.value = inputValue;
+}
