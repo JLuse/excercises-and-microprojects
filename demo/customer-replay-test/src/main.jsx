ca9e9eed74a412e7c9a8929b2be71914a6eee407
Jordan <j.luse7@gmail.com>
2023-12-04 10:20:02 -0800

    Replay tests

---
+++ demo/customer-replay-test/src/main.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://8481fafed26aaf6a604563eedd080b79@o1184810.ingest.sentry.io/4506322046746624",
+  integrations: [
+    new Sentry.BrowserTracing({
+      // See docs for support of different versions of variation of react router
+      // https://docs.sentry.io/platforms/javascript/guides/react/configuration/integrations/react-router/
+      // routingInstrumentation: Sentry.reactRouterV6Instrumentation(
+      //   React.useEffect,
+      //   useLocation,
+      //   useNavigationType,
+      //   createRoutesFromChildren,
+      //   matchRoutes
+      // ),
+    }),
+    new Sentry.Replay()
+  ],
+  debug: true,
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  tracesSampleRate: 1.0,
+
+  // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+
+  // Capture Replay for 10% of all sessions,
+  // plus for 100% of sessions with an error
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
