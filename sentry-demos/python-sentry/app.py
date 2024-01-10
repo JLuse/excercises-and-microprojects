f7bc7c2d5081502e684c0b92663f01bb4858dfe2
Jordan <j.luse7@gmail.com>
2024-01-10 12:49:20 -0800

    Pushing auth and sentry tests so i cna use a different computer

--- sentry-demos/python-sentry/app.py
+++ sentry-demos/python-sentry/app.py
@@ -4,7 +4,6 @@ from sentry_sdk import capture_message
 sentry_sdk.init(
     dsn="https://ca35d2e6803c4f98b3d27f353a15bf91@o565143.ingest.sentry.io/4504079916269568",
 
-    # Enable performance monitoring
     debug=True,
     enable_tracing=True,
 )
@@ -12,5 +11,3 @@ sentry_sdk.init(
 
 capture_message('Something went wrong')
 division_by_zero = 1 / 0
-
-# flush()
