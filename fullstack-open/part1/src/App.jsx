e0774866b829c9b716b08395a2ee97cb9e9f0dae
Jordan Luse <j.luse7@gmail.com>
2024-07-25 08:55:00 -0700

    Part 1c, fullstack open, 100devs class 16

--- fullstack-open/part1/src/App.jsx
+++ fullstack-open/part1/src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 const Header = (props) => {
   return (
     <h1>{props.course}</h1>
@@ -26,11 +28,37 @@ const Total = (props) => {
   )
 }
 
+const Hello = ({name, age }) => {
+  const bornYear = () => new Date().getFullYear() - age
 
+  return (
+    <div>
+      <p>
+        Hello {name}, you are {age} years old
+      </p>
 
+      <p>So you were probably born in {bornYear()}</p>
+    </div>
+  )
+}
 
+const Display = ({counter}) => <div>{counter}</div>
+
+const Button = ({onClick, text}) => {
+  return (
+    <button onClick={onClick}>
+      {text}
+    </button>
+  )
+}
 
 const App = () => {
+  const [ counter, setCounter ] = useState(0)
+
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
+
   const course = {
     name: 'Half Stack application development',
     parts: [
@@ -49,13 +77,29 @@ const App = () => {
     ]
   }
 
+  const people = {
+    persons: [
+      {
+        name: 'Jay',
+        age: 35
+      },
+    ]
+  }
 
+
+  
 
   return (
     <div>
       <Header course={course.name} />
       <Content parts={course.parts} />
       <Total parts={course.parts} />
+      <h1>HELLO BELOW:</h1>
+      <Hello name={people.persons[0].name} age={people.persons[0].age}/>
+      <Display counter={counter}/>
+      <Button onClick={increaseByOne} text='plus'/>
+      <Button onClick={decreaseByOne} text='minus'/>
+      <Button onClick={setToZero} text='zero'/>
     </div>
   )
 }
