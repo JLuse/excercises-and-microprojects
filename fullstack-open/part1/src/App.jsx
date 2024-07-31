7f27b9951c5ca5a227b7ac6d5955a7e84010232e
Jordan Luse <j.luse7@gmail.com>
2024-07-30 22:08:58 -0700

    Testing more state

--- fullstack-open/part1/src/App.jsx
+++ fullstack-open/part1/src/App.jsx
@@ -54,6 +54,9 @@ const Button = ({onClick, text}) => {
 
 const App = () => {
   const [ counter, setCounter ] = useState(0)
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+
 
   const increaseByOne = () => setCounter(counter + 1)
   const decreaseByOne = () => setCounter(counter - 1)
@@ -100,6 +103,17 @@ const App = () => {
       <Button onClick={increaseByOne} text='plus'/>
       <Button onClick={decreaseByOne} text='minus'/>
       <Button onClick={setToZero} text='zero'/>
+
+      <div>
+        {left}
+        <button onClick={() => setLeft(left + 1)}>
+          left
+        </button>
+        <button onClick={() => setRight(right + 1)}>
+          right
+        </button>
+        {right}
+      </div>
     </div>
   )
 }
