d6c0dab7203edd1335e044b6301e115c75fc8788
Jordan <j.luse7@gmail.com>
2024-08-13 21:53:58 -0700

    Finished golang comments, now on to numbers

--- exercism/go/weather-forecast/weather_forecast.go
+++ exercism/go/weather-forecast/weather_forecast.go
@@ -1,10 +1,12 @@
 // Package weather provides way of getting temapature.
 package weather
 
-
+// CurrentCondition is variable that holds a string to represent a condition.
 var CurrentCondition string
+// CurrentLocation variable that holds a string to represent a location.
 var CurrentLocation string
 
+// Forecast assigns the city and condition paramter to the CurrentLocation, CurrentCondition then returns a concatinatated version of those strings.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
